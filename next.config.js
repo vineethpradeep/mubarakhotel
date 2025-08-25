103ed15cfba86a22edf46a29544501b74e173f86
vineethpradeep <vineethpadeeps@gmail.com>
2025-08-25 17:03:31 +0100

    build github fix relative path css

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   output: "export",
-  //   assetPrefix: "/",
-  //   trailingSlash: true,
-  //   experimental: {
-  //     appDir: true,
-  //   },
   output: "export",
-  basePath: "/mubarakhotel",
-  assetPrefix: "/mubarakhotel/",
+  assetPrefix: "/",
   trailingSlash: true,
 };
 
